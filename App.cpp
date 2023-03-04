e354349a1a500e61f6f9e383b81ad692efa05093
Kushal Pathak <kushalpathak80@gmail.com>
2023-03-04 22:27:24 +0545

    minor update regarding ball and block and binding ball and unbinding ball

--- App.cpp
+++ App.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
-#define h 25
-#define w 50
+#define h 21
+#define w 51
+#define block (char)254
+#define ball_body 'O';
 using namespace std;
 
 char buffer[h][w];
+struct Ball {
+	int x = w / 2, y = h / 2;
+};
+
+Ball ball;
 
 void init_buffer();
+void bind_ball();
+void unbind_ball();
 void render();
 
 int main() {
 	init_buffer();
+	bind_ball();
 	render();
 }
-
+void bind_ball() {
+	buffer[ball.y][ball.x] = ball_body;
+}
+void unbind_ball() {
+	buffer[ball.y][ball.x] = ' ';
+}
 void init_buffer() {
 	for (int i = 0; i < h; i++) {
 		for (int j = 0; j < w; j++) {
-			buffer[i][j] = '*';
+			buffer[i][j] = ' ';
+			buffer[0][j] = '-';
+			buffer[h - 1][j] = '_';
 		}
 	}
 }
