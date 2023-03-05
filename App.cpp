7755de4d4adee2814aaf0c7db837b053e83b3811
Kushal Pathak <kushalpathak80@gmail.com>
2023-03-05 17:58:13 +0545

    little progress

--- App.cpp
+++ App.cpp
@@ -8,13 +8,13 @@
 #define ball_body 'O';
 using namespace std;
 
-char buffer[h][w];
+char buffer[h][w], play='y';
 struct Ball {
-	int x = w / 2, y = h / 2;
+	float x = w / 2, y = h / 2;
 };
-int step = 0, delay = 150;
-
 Ball ball;
+int step = 0, vy = 1, game_over = 0, jump_limit = ball.y - 3;
+int delay = 200;
 
 void init_buffer();
 void bind_ball();
@@ -23,19 +23,70 @@ void render();
 void generate_obstacle();
 void shift_obstacles();
 void update();
+void control();
+void detect_collision();
+void game_over_message();
+void reset_game();
 
 int main() {
 	srand((unsigned int)time(0));
-	init_buffer();
-	bind_ball();
-	while (1) {
-		render();
-		update();
-		//Sleep(delay);
+	while (play == 'y') {
+		reset_game();
+		while (!game_over) {
+			render();
+			control();
+			update();
+			detect_collision();
+			//Sleep(delay);
+		}
+		game_over_message();
 	}
 }
 
+void reset_game() {
+	game_over = 0;
+	play = 'y';
+	vy = 1;
+	step = 0;
+	ball.x = w / 2; ball.y = h / 2;
+	step = 0;
+	init_buffer();
+	bind_ball();
+}
+void game_over_message() {
+	cout << "Game Over!" << endl << "Play again (y): ";
+	play = 'n';
+	cin >> play;
+}
+void detect_collision() {
+	int tx = (int)round(ball.x);
+	int ty = (int)round(ball.y);
+	if (buffer[ty][tx] == block) {
+		game_over = 1;
+	}
+	if (ty <= 0 || ty >= h - 1) {
+		game_over = 1;
+	}
+}
+
+void control() {
+	if (_kbhit()) {
+		char c = _getch();
+		if (c == 'w') {
+			vy = -1;
+			jump_limit = ball.y - 3;
+		}
+	}
+
+}
+
 void update() {
+	if (ball.y == jump_limit) {
+		vy = 1;
+	}
+	unbind_ball();
+	ball.y += vy;
+	bind_ball();
 	shift_obstacles();
 	generate_obstacle();
 	step++;
@@ -65,10 +116,14 @@ void generate_obstacle() {
 	}
 }
 void bind_ball() {
-	buffer[ball.y][ball.x] = ball_body;
+	int tx = (int)round(ball.x);
+	int ty = (int)round(ball.y);
+	buffer[ty][tx] = ball_body;
 }
 void unbind_ball() {
-	buffer[ball.y][ball.x] = ' ';
+	int tx = (int)round(ball.x);
+	int ty = (int)round(ball.y);
+	buffer[ty][tx] = ' ';
 }
 void init_buffer() {
 	for (int i = 0; i < h; i++) {
